5efa4a383f3dad425769725f704111a9c4bfb119
Damian Lasecki <damian.x.lasecki@gsk.com>
2019-12-29 08:55:57 +0100

    Initial Commit

---
+++ ProjectNo2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectNo2
+//
+//  Created by Damian Lasecki on 29/12/2019.
+//  Copyright © 2019 Damian Lasecki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
